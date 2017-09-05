140fe211f24503faf03fd6ff4a50ea026b771b94
Flávio Pontes <flavio.pontes@siteblindado.com.br>
2017-09-05 14:59:36 -0300

    #1 Added root field adition when passing a dict to the logging function.

--- simple_json_formatter/formatter.py
+++ simple_json_formatter/formatter.py
@@ -1,8 +1,5 @@
 import logging
-try:
-    from logging import _levelToName
-except ImportError:
-    from logging import _levelNames as _levelToName
+from logging import _levelToName
 
 import datetime
 import traceback
@@ -24,12 +21,11 @@ class JsonFormatter(logging.Formatter):
             tb = ''.join(traceback.format_tb(obj))
             return tb.strip()
         elif isinstance(obj, Exception):
-            return "Exception: %s" % str(obj)
+            return "Exception: {}".format(str(obj))
         return str(obj)
 
-    def format(self, record, extra=None):
+    def format(self, record):
         msg = {
-            'msg': record.msg,
             'logged_at': datetime.datetime.now().isoformat(),
             'line_number': record.lineno,
             'function': record.funcName,
@@ -37,8 +33,16 @@ class JsonFormatter(logging.Formatter):
             'path': record.pathname
         }
 
-        if extra:
-            msg.update(extra)
+        defaulf_log_record_fields = {'name', 'msg', 'args', 'levelname', 'levelno', 'pathname', 'filename', 'module', 'exc_info', 'exc_text', 'stack_info', 'lineno', 'funcName', 'created', 'msecs', 'relativeCreated', 'thread', 'threadName', 'processName', 'process'}
+
+        for field, value in record.__dict__.items():
+            if field not in defaulf_log_record_fields:
+                msg[field] = value
+
+        if isinstance(record.msg, dict):
+            msg.update(record.msg)
+        else:
+            msg['msg'] = record.msg
 
         if record.exc_info:
             msg['exc_info'] = record.exc_info
--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -4,12 +4,6 @@ import json
 import inspect
 from datetime import datetime
 from io import StringIO
-from sys import stdout
-try:
-    from unittest.mock import Mock, call, patch
-except ImportError:
-    # python 2.7
-    from mock import Mock, call, patch
 
 from simple_json_formatter import JsonFormatter
 from freezegun import freeze_time
@@ -26,7 +20,7 @@ class LoggerTests(unittest.TestCase):
 
     def test_it_logs_valid_json_string_if_message_is_json_serializeable(self):
         message = {
-            'info': 'Se tem permissao, tamo dando sarrada',
+            'info': 'Se tem permissao, vamos nessa',
             'msg': {
                 'foo': 'bar',
                 'baz': 'blu'
@@ -38,7 +32,7 @@ class LoggerTests(unittest.TestCase):
         logged_content = self.buffer.getvalue()
         json_log = json.loads(logged_content)
 
-        self.assertDictEqual(json_log['msg'], message)
+        self.assertDictContainsSubset(message, json_log)
 
     def test_it_logs_valid_json_string_if_message_isnt_json_serializeable(self):
         class FooJsonUnserializeable:
@@ -51,7 +45,7 @@ class LoggerTests(unittest.TestCase):
         logged_content = self.buffer.getvalue()
         json_log = json.loads(logged_content)
 
-        self.assertDictEqual(json_log['msg'], {'info': str(obj)})
+        self.assertDictContainsSubset({'info': str(obj)}, json_log)
 
     def test_it_escapes_strings(self):
         message = """"Aaaalgma coisa"paando `bem por'/\t \\" \" \' \n "aaa """
@@ -111,7 +105,7 @@ class LoggerTests(unittest.TestCase):
             'time': message['time'].isoformat(),
             'datetime': message['datetime'].isoformat()
         }
-        self.assertDictEqual(json_log['msg'], expected_output)
+        self.assertDictContainsSubset(expected_output, json_log)
 
     def test_extra_fields(self):
         logging.info('Isto é um teste', extra={'field_1': 'field_1 content',
@@ -128,3 +122,4 @@ class LoggerTests(unittest.TestCase):
             'field_3': 'field_3 content'
         }
         self.assertDictContainsSubset(expected_output, json_log)
+
